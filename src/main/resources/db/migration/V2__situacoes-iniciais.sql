3cc837afa27b55b1cc98c793326a80f6a25de524
André Lima <euandrelima@gmail.com>
2023-08-11 10:50:33 -0300

    Ajuste na descrição do ID da situação Bloqueado

--- src/main/resources/db/migration/V2__situacoes-iniciais.sql
+++ src/main/resources/db/migration/V2__situacoes-iniciais.sql
@@ -1,2 +1,2 @@
 insert into situacao_cartorio values ('SIT_ATIVO', 'ATIVO');
-insert into situacao_cartorio values ('aoSIT_BLOQUEADO', 'BLOQUEADO');
+insert into situacao_cartorio values ('SIT_BLOQUEADO', 'BLOQUEADO');
