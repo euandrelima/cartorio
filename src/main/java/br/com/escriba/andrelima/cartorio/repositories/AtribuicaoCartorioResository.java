b7cc012dff219b54b8537d660d6b252b72956c26
André Lima <euandrelima@gmail.com>
2023-08-13 17:32:45 -0300

    Implementação do endpoint create de Atribuição

--- src/main/java/br/com/escriba/andrelima/cartorio/repositories/AtribuicaoCartorioResository.java
+++ src/main/java/br/com/escriba/andrelima/cartorio/repositories/AtribuicaoCartorioResository.java
@@ -4,6 +4,9 @@ import br.com.escriba.andrelima.cartorio.domain.AtribuicaoCartorio;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AtribuicaoCartorioResository extends PagingAndSortingRepository<AtribuicaoCartorio, String> {
+    Optional<AtribuicaoCartorio> findByNome(String nome);
 }
--- src/main/java/br/com/escriba/andrelima/cartorio/services/AtribuicaoCartorioService.java
+++ src/main/java/br/com/escriba/andrelima/cartorio/services/AtribuicaoCartorioService.java
@@ -1,7 +1,9 @@
 package br.com.escriba.andrelima.cartorio.services;
 
 import br.com.escriba.andrelima.cartorio.domain.AtribuicaoCartorio;
+import br.com.escriba.andrelima.cartorio.domain.dtos.AtribuicaoCartorioDTO;
 import br.com.escriba.andrelima.cartorio.repositories.AtribuicaoCartorioResository;
+import br.com.escriba.andrelima.cartorio.services.execeptions.DuplicateObjectException;
 import br.com.escriba.andrelima.cartorio.services.execeptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -22,7 +24,28 @@ public class AtribuicaoCartorioService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Atribuição não encontrada! Código: " + id));
     }
 
+    public AtribuicaoCartorio create(AtribuicaoCartorioDTO objDTO) {
+        verificaIdExistente(objDTO);
+        verificaNomeExistente(objDTO);
+        AtribuicaoCartorio newObj = new AtribuicaoCartorio(objDTO);
+        return repository.save(newObj);
+    }
+
     public Page<AtribuicaoCartorio> findAll(Pageable pageable) {
         return repository.findAll(pageable);
+    }
+
+    private void verificaIdExistente(AtribuicaoCartorioDTO objDTO) {
+        Optional<AtribuicaoCartorio> obj = repository.findById(objDTO.getId());
+        if(obj.isPresent() && obj.get().getId() == objDTO.getId()) {
+            throw new DuplicateObjectException("Registro já cadastrado");
+        }
+    }
+
+    private void verificaNomeExistente(AtribuicaoCartorioDTO objDTO) {
+        Optional<AtribuicaoCartorio> obj = repository.findByNome(objDTO.getNome());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw  new DuplicateObjectException("Nome já informado no registro com código " + obj.get().getId());
+        }
     }
 }
